zeeke/sriov-cni

commit c241dcb4367c14578b57dd139079bde1f6e997c2

Send IPv4 GARP and IPv6 Unsolicited NA in "cmdAdd"
In "cmdAdd", SRIOV-CNI would construct and send IPv4 Gratuitous ARP
and/or Unsolicited Neighbor Advertisement depending on the IP
addresses configured by IPAM. The reason why this change is needed
is for the scenario when an IP address is reused by IPAM with
different interfaces (with different link-layer addresses). This can
occur when pods are deleted and created. For performance reasons,
sending of GARP and/or Unsolicited NA would update invalid ARP/Neighbor
caches in other neighbors/nodes.

Also we set IPv4 ARP Notify and IPv6 Neighbor Discovery Notify in sysfs
for each interface. This will send GARP and/or Unsolicited NA when the
interface is either brought up or the link-layer address changes. This
is useful in cases where an application reenables the interface or the
MAC address configuration is changed.

Some new packages were added, thus go.mod and go.sum were modified
accordingly.

Mocked PciUtils for sriov tests since sriov.go would call PciUtils to
set IPv4 ARP Notify and IPv6 Neighbor Discovery.

Fixes k8snetworkplumbingwg#177

Signed-off-by: William Zhao <wizhao@redhat.com>

cmd/sriov/main.go
@@ -135,7 +135,27 @@ func cmdAdd(args *skel.CmdArgs) error {
 
 		if !netConf.DPDKMode {
 			err = netns.Do(func(_ ns.NetNS) error {
-				return ipam.ConfigureIface(args.IfName, newResult)
+				err := ipam.ConfigureIface(args.IfName, newResult)
+				if err != nil {
+					return err
+				}
+
+				/* After IPAM configuration is done, the following needs to handle the case of an IP address being reused by a different pods.
+				 * This is achieved by sending Gratuitous ARPs and/or Unsolicited Neighbor Advertisements unconditionally.
+				 * Although we set arp_notify and ndisc_notify unconditionally on the interface (please see EnableArpAndNdiscNotify()), the kernel
+				 * only sends GARPs/Unsolicited NA when the interface goes from down to up, or when the link-layer address changes on the interfaces.
+				 * These scenarios are perfectly valid and recommended to be enabled for optimal network performance.
+				 * However for our specific case, which the kernel is unaware of, is the reuse of IP addresses across pods where each pod has a different
+				 * link-layer address for it's SRIOV interface. The ARP/Neighbor cache residing in neighbors would be invalid if an IP address is reused.
+				 * In order to update the cache, the GARP/Unsolicited NA packets should be sent for performance reasons. Otherwise, the neighbors
+				 * may be sending packets with the incorrect link-layer address. Eventually, most network stacks would send ARPs and/or Neighbor
+				 * Solicitation packets when the connection is unreachable. This would correct the invalid cache; however this may take a significant
+				 * amount of time to complete.
+				 *
+				 * The error is ignored here because enabling this feature is only a performance enhancement.
+				 */
+				_ = utils.AnnounceIPs(args.IfName, newResult.IPs)
+				return nil
 			})
 			if err != nil {
 				return err

pkg/utils/packet.go
@@ -1,0 +1,198 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"syscall"
+
+	"github.com/containernetworking/cni/pkg/types/current"
+	"github.com/vishvananda/netlink"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv6"
+)
+
+var (
+	arpPacketName    = "ARP"
+	icmpV6PacketName = "ICMPv6"
+)
+
+// htons converts an uint16 from host to network byte order.
+func htons(i uint16) uint16 {
+	return (i<<8)&0xff00 | i>>8
+}
+
+// formatPacketFieldWriteError builds an error string for the cases when writing to a field of a packet fails.
+func formatPacketFieldWriteError(field string, packetType string, writeErr error) error {
+	return fmt.Errorf("failed to write the %s field in the %s packet: %v", field, packetType, writeErr)
+}
+
+// SendGratuitousArp sends a gratuitous ARP packet with the provided source IP over the provided interface.
+func SendGratuitousArp(srcIP net.IP, linkObj netlink.Link) error {
+	/* As per RFC 5944 section 4.6, a gratuitous ARP packet can be sent by a node in order to spontaneously cause other nodes to update
+	 * an entry in their ARP cache. In the case of SRIOV-CNI, an address can be reused for different pods. Each pod could likely have a
+	 * different link-layer address in this scenario, which makes the ARP cache entries residing in the other nodes to be an invalid.
+	 * The gratuitous ARP packet should update the link-layer address accordingly for the invalid ARP cache.
+	 */
+
+	// Construct the ARP packet following RFC 5944 section 4.6.
+	arpPacket := new(bytes.Buffer)
+	if writeErr := binary.Write(arpPacket, binary.BigEndian, uint16(1)); writeErr != nil { // Hardware Type: 1 is Ethernet
+		return formatPacketFieldWriteError("Hardware Type", arpPacketName, writeErr)
+	}
+	if writeErr := binary.Write(arpPacket, binary.BigEndian, uint16(syscall.ETH_P_IP)); writeErr != nil { // Protocol Type: 0x0800 is IPv4
+		return formatPacketFieldWriteError("Protocol Type", arpPacketName, writeErr)
+	}
+	if writeErr := binary.Write(arpPacket, binary.BigEndian, uint8(6)); writeErr != nil { // Hardware address Length: 6 bytes for MAC address
+		return formatPacketFieldWriteError("Hardware address Length", arpPacketName, writeErr)
+	}
+	if writeErr := binary.Write(arpPacket, binary.BigEndian, uint8(4)); writeErr != nil { // Protocol address length: 4 bytes for IPv4 address
+		return formatPacketFieldWriteError("Protocol address length", arpPacketName, writeErr)
+	}
+	if writeErr := binary.Write(arpPacket, binary.BigEndian, uint16(1)); writeErr != nil { // Operation: 1 is request, 2 is response
+		return formatPacketFieldWriteError("Operation", arpPacketName, writeErr)
+	}
+	if _, writeErr := arpPacket.Write(linkObj.Attrs().HardwareAddr); writeErr != nil { // Sender hardware address
+		return formatPacketFieldWriteError("Sender hardware address", arpPacketName, writeErr)
+	}
+	if _, writeErr := arpPacket.Write(srcIP.To4()); writeErr != nil { // Sender protocol address
+		return formatPacketFieldWriteError("Sender protocol address", arpPacketName, writeErr)
+	}
+	if _, writeErr := arpPacket.Write([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}); writeErr != nil { // Target hardware address is the Broadcast MAC.
+		return formatPacketFieldWriteError("Target hardware address", arpPacketName, writeErr)
+	}
+	if _, writeErr := arpPacket.Write(srcIP.To4()); writeErr != nil { // Target protocol address
+		return formatPacketFieldWriteError("Target protocol address", arpPacketName, writeErr)
+	}
+
+	sockAddr := syscall.SockaddrLinklayer{
+		Protocol: htons(syscall.ETH_P_ARP),                                // Ethertype of ARP (0x0806)
+		Ifindex:  linkObj.Attrs().Index,                                   // Interface Index
+		Hatype:   1,                                                       // Hardware Type: 1 is Ethernet
+		Pkttype:  0,                                                       // Packet Type.
+		Halen:    6,                                                       // Hardware address Length: 6 bytes for MAC address
+		Addr:     [8]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, // Address is the broadcast MAC address.
+	}
+
+	// Create a socket such that the Ethernet header would constructed by the OS. The arpPacket only contains the ARP payload.
+	soc, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_DGRAM, int(htons(syscall.ETH_P_ARP)))
+	if err != nil {
+		return fmt.Errorf("failed to create AF_PACKET datagram socket: %v", err)
+	}
+	defer syscall.Close(soc)
+
+	if err := syscall.Sendto(soc, arpPacket.Bytes(), 0, &sockAddr); err != nil {
+		return fmt.Errorf("failed to send Gratuitous ARP for IPv4 %s on Interface %s: %v", srcIP.String(), linkObj.Attrs().Name, err)
+	}
+
+	return nil
+}
+
+// SendUnsolicitedNeighborAdvertisement sends an unsolicited neighbor advertisement packet with the provided source IP over the provided interface.
+func SendUnsolicitedNeighborAdvertisement(srcIP net.IP, linkObj netlink.Link) error {
+	/* As per RFC 4861, a link-layer address change can multicast a few unsolicited neighbor advertisements to all nodes to quickly
+	 * update the cached link-layer addresses that have become invalid. In the case of SRIOV-CNI, an address can be reused for
+	 * different pods. Each pod could likely have a different link-layer address in this scenario, which makes the Neighbor Cache
+	 * entries residing in the neighbors to be an invalid. The unsolicited neighbor advertisement should update the link-layer address
+	 * accordingly for the IPv6 entry.
+	 * However if any of these conditions are true:
+	 *  - The IPv6 address was not reused for the new pod.
+	 *  - No prior established communication with the neighbor.
+	 * Then the neighbor receiving this unsolicited neighbor advertisement would be silently discard. This behavior is described
+	 * in RFC 4861 section 7.2.5. This is acceptable behavior since the purpose of sending an unsolicited neighbor advertisement
+	 * is not to create a new entry but rather update already existing invalid entries.
+	 */
+
+	// Construct the ICMPv6 Neighbor Advertisement packet following RFC 4861.
+	payload := new(bytes.Buffer)
+	// ICMPv6 Flags: As per RFC 4861, the solicited flag must not be set and the override flag should be set (to
+	// override existing cache entry) for unsolicited advertisements.
+	if writeErr := binary.Write(payload, binary.BigEndian, uint32(0x20000000)); writeErr != nil {
+		return formatPacketFieldWriteError("Flags", icmpV6PacketName, writeErr)
+	}
+	if _, writeErr := payload.Write(srcIP.To16()); writeErr != nil { // ICMPv6 Target IPv6 Address.
+		return formatPacketFieldWriteError("Target IPv6 Address", icmpV6PacketName, writeErr)
+	}
+	if writeErr := binary.Write(payload, binary.BigEndian, uint8(2)); writeErr != nil { // ICMPv6 Option Type: 2 is target link-layer address.
+		return formatPacketFieldWriteError("Option Type", icmpV6PacketName, writeErr)
+	}
+	if writeErr := binary.Write(payload, binary.BigEndian, uint8(1)); writeErr != nil { // ICMPv6 Option Length. Units of 8 bytes.
+		return formatPacketFieldWriteError("Option Length", icmpV6PacketName, writeErr)
+	}
+	if _, writeErr := payload.Write(linkObj.Attrs().HardwareAddr); writeErr != nil { // ICMPv6 Option Link-layer Address.
+		return formatPacketFieldWriteError("Option Link-layer Address", icmpV6PacketName, writeErr)
+	}
+
+	icmpv6Msg := icmp.Message{
+		Type:     ipv6.ICMPTypeNeighborAdvertisement, // ICMPv6 type is neighbor advertisement.
+		Code:     0,                                  // ICMPv6 Code: As per RFC 4861 section 7.1.2, the code is always 0.
+		Checksum: 0,                                  // Checksum is calculated later.
+		Body: &icmp.RawBody{
+			Data: payload.Bytes(),
+		},
+	}
+
+	// Get the byte array of the ICMPv6 Message.
+	icmpv6Bytes, err := icmpv6Msg.Marshal(nil)
+	if err != nil {
+		return fmt.Errorf("failed to Marshal ICMPv6 Message: %v", err)
+	}
+
+	// Create a socket such that the Ethernet header and IPv6 header would constructed by the OS.
+	soc, err := syscall.Socket(syscall.AF_INET6, syscall.SOCK_RAW, syscall.IPPROTO_ICMPV6)
+	if err != nil {
+		return fmt.Errorf("failed to create AF_INET6 raw socket: %v", err)
+	}
+	defer syscall.Close(soc)
+
+	// As per RFC 4861 section 7.1.2, the IPv6 hop limit is always 255.
+	if err := syscall.SetsockoptInt(soc, syscall.IPPROTO_IPV6, syscall.IPV6_MULTICAST_HOPS, 255); err != nil {
+		return fmt.Errorf("failed to set IPv6 multicast hops to 255: %v", err)
+	}
+
+	// Set the destination IPv6 address to the IPv6 link-local all nodes multicast address (ff02::1).
+	var r [16]byte
+	copy(r[:], net.IPv6linklocalallnodes.To16())
+	sockAddr := syscall.SockaddrInet6{Addr: r}
+	if err := syscall.Sendto(soc, icmpv6Bytes, 0, &sockAddr); err != nil {
+		return fmt.Errorf("failed to send Unsolicited Neighbor Advertisement for IPv6 %s on Interface %s: %v", srcIP.String(), linkObj.Attrs().Name, err)
+	}
+
+	return nil
+}
+
+// AnnounceIPs sends either a GARP or Unsolicited NA depending on the IP address type (IPv4 vs. IPv6 respectively) configured on the interface.
+func AnnounceIPs(ifName string, ipConfigs []*current.IPConfig) error {
+	myNetLink := MyNetlink{}
+
+	// Retrieve the interface name in the container.
+	linkObj, err := myNetLink.LinkByName(ifName)
+	if err != nil {
+		return fmt.Errorf("failed to get netlink device with name %q: %v", ifName, err)
+	}
+	if !IsValidMACAddress(linkObj.Attrs().HardwareAddr) {
+		return fmt.Errorf("invalid Ethernet MAC address: %q", linkObj.Attrs().HardwareAddr)
+	}
+
+	// For all the IP addresses assigned by IPAM, we will send either a GARP (IPv4) or Unsolicited NA (IPv6).
+	for _, ipc := range ipConfigs {
+		var err error
+		if IsIPv6(ipc.Address.IP) {
+			/* As per RFC 4861, sending unsolicited neighbor advertisements should be considered as a performance
+			* optimization. It does not reliably update caches in all nodes. The Neighbor Unreachability Detection
+			* algorithm is more reliable although it may take slightly longer to update.
+			 */
+			err = SendUnsolicitedNeighborAdvertisement(ipc.Address.IP, linkObj)
+		} else if IsIPv4(ipc.Address.IP) {
+			err = SendGratuitousArp(ipc.Address.IP, linkObj)
+		} else {
+			return fmt.Errorf("the IP %s on interface %q is neither IPv4 or IPv6", ipc.Address.IP.String(), ifName)
+		}
+
+		if err != nil {
+			return fmt.Errorf("failed to send GARP/NA message for ip %s on interface %q: %v", ipc.Address.IP.String(), ifName, err)
+		}
+	}
+	return nil
+}

go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/onsi/gomega v0.0.0-20151007035656-2152b45fa28a
 	github.com/stretchr/testify v1.4.0
 	github.com/vishvananda/netlink v1.0.1-0.20190924205540-07ace697bea4
+	golang.org/x/net v0.0.0-20220802222814-0bcc04d9c69b
 )
 
 require (
@@ -18,6 +19,6 @@ require (
 	github.com/safchain/ethtool v0.0.0-20190326074333-42ed695e3de8 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
 	github.com/vishvananda/netns v0.0.0-20180720170159-13995c7128cc // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )

go.sum
@@ -43,9 +43,15 @@ github.com/vishvananda/netns v0.0.0-20180720170159-13995c7128cc h1:R83G5ikgLMxrB
 github.com/vishvananda/netns v0.0.0-20180720170159-13995c7128cc/go.mod h1:ZjcWmFBXmLKZu9Nxj3WKYEafiSqer2rnvPr0en9UNpI=
 golang.org/x/crypto v0.0.0-20181009213950-7c1a557ab941/go.mod h1:6SG95UA2DQfeDnfUPMdvaQW0Q7yPrPDi9nlGo2tz2b4=
 golang.org/x/net v0.0.0-20181011144130-49bb7cea24b1/go.mod h1:mL1N/T3taQHkDXs73rZJwtUhF3w3ftmwwsq0BUmARs4=
+golang.org/x/net v0.0.0-20220802222814-0bcc04d9c69b h1:3ogNYyK4oIQdIKzTu68hQrr4iuVxF3AxKl9Aj/eDrw0=
+golang.org/x/net v0.0.0-20220802222814-0bcc04d9c69b/go.mod h1:YDH+HFinaLZZlnHAfSS6ZXJJ9M9t4Dl22yv3iI2vPwk=
 golang.org/x/sys v0.0.0-20190616124812-15dcb6c0061f/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
-golang.org/x/sys v0.0.0-20210510120138-977fb7262007 h1:gG67DSER+11cZvqIMb8S8bt0vZtiN6xWYARwirrOSfE=
-golang.org/x/sys v0.0.0-20210510120138-977fb7262007/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
+golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
+golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 h1:WIoqL4EROvwiPdUtaip4VcDdpZ4kha7wBWZrbVKCIZg=
+golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
+golang.org/x/term v0.0.0-20210927222741-03fcf44c2211/go.mod h1:jbD1KX2456YbFQfuXm/mYQcufACuNUgVhRMnK/tPxf8=
+golang.org/x/text v0.3.7/go.mod h1:u+2+/6zg+i71rQMx5EYifcz6MCKuco9NR6JIITiCfzQ=
+golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e/go.mod h1:n7NCudcB/nEzxVGmLbDWY5pfWTLqBcC2KZ6jyYvM4mQ=
 gopkg.in/airbrake/gobrake.v2 v2.0.9/go.mod h1:/h5ZAUhDkGaJfjzjKLSjv6zCL6O0LLBxU4K+aSYdM/U=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 h1:yhCVgyC4o1eVCa2tZl7eS0r+SDo693bJlVdllGtEeKM=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405/go.mod h1:Co6ibVJAznAaIkqp8huTwlJQCZ016jof/cbN4VW5Yz0=

pkg/sriov/mocks/pci_utils_mock.go
@@ -1,0 +1,104 @@
+// Code generated by mockery v2.14.0. DO NOT EDIT.
+
+package mocks
+
+import mock "github.com/stretchr/testify/mock"
+
+// PciUtils is an autogenerated mock type for the pciUtils type
+type PciUtils struct {
+	mock.Mock
+}
+
+// EnableArpAndNdiscNotify provides a mock function with given fields: ifName
+func (_m *PciUtils) EnableArpAndNdiscNotify(ifName string) error {
+	ret := _m.Called(ifName)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(string) error); ok {
+		r0 = rf(ifName)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// GetPciAddress provides a mock function with given fields: ifName, vf
+func (_m *PciUtils) GetPciAddress(ifName string, vf int) (string, error) {
+	ret := _m.Called(ifName, vf)
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func(string, int) string); ok {
+		r0 = rf(ifName, vf)
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, int) error); ok {
+		r1 = rf(ifName, vf)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// GetSriovNumVfs provides a mock function with given fields: ifName
+func (_m *PciUtils) GetSriovNumVfs(ifName string) (int, error) {
+	ret := _m.Called(ifName)
+
+	var r0 int
+	if rf, ok := ret.Get(0).(func(string) int); ok {
+		r0 = rf(ifName)
+	} else {
+		r0 = ret.Get(0).(int)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string) error); ok {
+		r1 = rf(ifName)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// GetVFLinkNamesFromVFID provides a mock function with given fields: pfName, vfID
+func (_m *PciUtils) GetVFLinkNamesFromVFID(pfName string, vfID int) ([]string, error) {
+	ret := _m.Called(pfName, vfID)
+
+	var r0 []string
+	if rf, ok := ret.Get(0).(func(string, int) []string); ok {
+		r0 = rf(pfName, vfID)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]string)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, int) error); ok {
+		r1 = rf(pfName, vfID)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+type mockConstructorTestingTNewPciUtils interface {
+	mock.TestingT
+	Cleanup(func())
+}
+
+// NewPciUtils creates a new instance of PciUtils. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
+func NewPciUtils(t mockConstructorTestingTNewPciUtils) *PciUtils {
+	mock := &PciUtils{}
+	mock.Mock.Test(t)
+
+	t.Cleanup(func() { mock.AssertExpectations(t) })
+
+	return mock
+}

pkg/sriov/sriov.go
@@ -11,114 +11,29 @@ import (
 	"github.com/vishvananda/netlink"
 )
 
-// mocked netlink interface
-// required for unit tests
-
-// NetlinkManager is an interface to mock nelink library
-type NetlinkManager interface {
-	LinkByName(string) (netlink.Link, error)
-	LinkSetVfVlan(netlink.Link, int, int) error
-	LinkSetVfVlanQos(netlink.Link, int, int, int) error
-	LinkSetVfHardwareAddr(netlink.Link, int, net.HardwareAddr) error
-	LinkSetHardwareAddr(netlink.Link, net.HardwareAddr) error
-	LinkSetUp(netlink.Link) error
-	LinkSetDown(netlink.Link) error
-	LinkSetNsFd(netlink.Link, int) error
-	LinkSetName(netlink.Link, string) error
-	LinkSetVfRate(netlink.Link, int, int, int) error
-	LinkSetVfSpoofchk(netlink.Link, int, bool) error
-	LinkSetVfTrust(netlink.Link, int, bool) error
-	LinkSetVfState(netlink.Link, int, uint32) error
-}
-
-// MyNetlink NetlinkManager
-type MyNetlink struct {
-	NetlinkManager
-}
-
-// LinkByName implements NetlinkManager
-func (n *MyNetlink) LinkByName(name string) (netlink.Link, error) {
-	return netlink.LinkByName(name)
-}
-
-// LinkSetVfVlan using NetlinkManager
-func (n *MyNetlink) LinkSetVfVlan(link netlink.Link, vf, vlan int) error {
-	return netlink.LinkSetVfVlan(link, vf, vlan)
-}
-
-// LinkSetVfVlanQos sets VLAN ID and QoS field for given VF using NetlinkManager
-func (n *MyNetlink) LinkSetVfVlanQos(link netlink.Link, vf, vlan, qos int) error {
-	return netlink.LinkSetVfVlanQos(link, vf, vlan, qos)
-}
-
-// LinkSetVfHardwareAddr using NetlinkManager
-func (n *MyNetlink) LinkSetVfHardwareAddr(link netlink.Link, vf int, hwaddr net.HardwareAddr) error {
-	return netlink.LinkSetVfHardwareAddr(link, vf, hwaddr)
-}
-
-// LinkSetHardwareAddr using NetlinkManager
-func (n *MyNetlink) LinkSetHardwareAddr(link netlink.Link, hwaddr net.HardwareAddr) error {
-	return netlink.LinkSetHardwareAddr(link, hwaddr)
-}
-
-// LinkSetUp using NetlinkManager
-func (n *MyNetlink) LinkSetUp(link netlink.Link) error {
-	return netlink.LinkSetUp(link)
-}
-
-// LinkSetDown using NetlinkManager
-func (n *MyNetlink) LinkSetDown(link netlink.Link) error {
-	return netlink.LinkSetDown(link)
-}
-
-// LinkSetNsFd using NetlinkManager
-func (n *MyNetlink) LinkSetNsFd(link netlink.Link, fd int) error {
-	return netlink.LinkSetNsFd(link, fd)
-}
-
-// LinkSetName using NetlinkManager
-func (n *MyNetlink) LinkSetName(link netlink.Link, name string) error {
-	return netlink.LinkSetName(link, name)
-}
-
-// LinkSetVfRate using NetlinkManager
-func (n *MyNetlink) LinkSetVfRate(link netlink.Link, vf int, minRate int, maxRate int) error {
-	return netlink.LinkSetVfRate(link, vf, minRate, maxRate)
-}
-
-// LinkSetVfSpoofchk using NetlinkManager
-func (n *MyNetlink) LinkSetVfSpoofchk(link netlink.Link, vf int, check bool) error {
-	return netlink.LinkSetVfSpoofchk(link, vf, check)
-}
-
-// LinkSetVfTrust using NetlinkManager
-func (n *MyNetlink) LinkSetVfTrust(link netlink.Link, vf int, state bool) error {
-	return netlink.LinkSetVfTrust(link, vf, state)
-}
-
-// LinkSetVfState using NetlinkManager
-func (n *MyNetlink) LinkSetVfState(link netlink.Link, vf int, state uint32) error {
-	return netlink.LinkSetVfState(link, vf, state)
-}
-
 type pciUtils interface {
-	getSriovNumVfs(ifName string) (int, error)
-	getVFLinkNamesFromVFID(pfName string, vfID int) ([]string, error)
-	getPciAddress(ifName string, vf int) (string, error)
+	GetSriovNumVfs(ifName string) (int, error)
+	GetVFLinkNamesFromVFID(pfName string, vfID int) ([]string, error)
+	GetPciAddress(ifName string, vf int) (string, error)
+	EnableArpAndNdiscNotify(ifName string) error
 }
 
 type pciUtilsImpl struct{}
 
-func (p *pciUtilsImpl) getSriovNumVfs(ifName string) (int, error) {
+func (p *pciUtilsImpl) GetSriovNumVfs(ifName string) (int, error) {
 	return utils.GetSriovNumVfs(ifName)
 }
 
-func (p *pciUtilsImpl) getVFLinkNamesFromVFID(pfName string, vfID int) ([]string, error) {
+func (p *pciUtilsImpl) GetVFLinkNamesFromVFID(pfName string, vfID int) ([]string, error) {
 	return utils.GetVFLinkNamesFromVFID(pfName, vfID)
 }
 
-func (p *pciUtilsImpl) getPciAddress(ifName string, vf int) (string, error) {
+func (p *pciUtilsImpl) GetPciAddress(ifName string, vf int) (string, error) {
 	return utils.GetPciAddress(ifName, vf)
+}
+
+func (p *pciUtilsImpl) EnableArpAndNdiscNotify(ifName string) error {
+	return utils.EnableArpAndNdiscNotify(ifName)
 }
 
 // Manager provides interface invoke sriov nic related operations
@@ -130,14 +45,14 @@ type Manager interface {
 }
 
 type sriovManager struct {
-	nLink NetlinkManager
+	nLink utils.NetlinkManager
 	utils pciUtils
 }
 
 // NewSriovManager returns an instance of SriovManager
 func NewSriovManager() Manager {
 	return &sriovManager{
-		nLink: &MyNetlink{},
+		nLink: &utils.MyNetlink{},
 		utils: &pciUtilsImpl{},
 	}
 }
@@ -192,7 +107,11 @@ func (s *sriovManager) SetupVF(conf *sriovtypes.NetConf, podifName string, cid s
 			return fmt.Errorf("error setting container interface name %s for %s", linkName, tempName)
 		}
 
-		// 6. Bring IF up in Pod netns
+		// 6. Enable IPv4 ARP notify and IPv6 Network Discovery notify
+		// Error is ignored here because enabling this feature is only a performance enhancement.
+		_ = s.utils.EnableArpAndNdiscNotify(podifName)
+
+		// 7. Bring IF up in Pod netns
 		if err := s.nLink.LinkSetUp(linkObj); err != nil {
 			return fmt.Errorf("error bringing interface up in container ns: %q", err)
 		}

commit d4e80b67c3bce38a32a631bf505c5d3cd1a5be78

Wait for carrier before announcing IPs via GARP/NA
After setting up the interface, it might take a bit for kernel to detect
carrier. If we then already send the GARP/NA packets, they are lost.

Instead, wait for up to 200 msec for the interface to get carrier. This
time is chosen somewhat arbitrarily. We don't want to block the process
too long, but we also need to wait long enough, that kernel and driver
has time to detect carrier. Also, while busy waiting, sleep with an
exponential back-off time (growth factor 1.5).

Fixes: c241dcb4367c ('Send IPv4 GARP and IPv6 Unsolicited NA in "cmdAdd"')

See-also: https://issues.redhat.com/browse/OCPBUGS-30549

Signed-off-by: Thomas Haller <thaller@redhat.com>

cmd/sriov/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/types"
@@ -205,10 +206,15 @@ func cmdAdd(args *skel.CmdArgs) error {
 			 * may be sending packets with the incorrect link-layer address. Eventually, most network stacks would send ARPs and/or Neighbor
 			 * Solicitation packets when the connection is unreachable. This would correct the invalid cache; however this may take a significant
 			 * amount of time to complete.
-			 *
-			 * The error is ignored here because enabling this feature is only a performance enhancement.
 			 */
-			_ = utils.AnnounceIPs(args.IfName, result.IPs)
+
+			/* The interface might not yet have carrier. Wait for it for a short time. */
+			hasCarrier := utils.WaitForCarrier(args.IfName, 200*time.Millisecond)
+
+			/* The error is ignored here because enabling this feature is only a performance enhancement. */
+			err = utils.AnnounceIPs(args.IfName, result.IPs)
+
+			logging.Debug("announcing IPs", "hasCarrier", hasCarrier, "IPs", result.IPs, "announceError", err)
 			return nil
 		})
 	}

pkg/utils/packet.go
@@ -6,11 +6,13 @@ import (
 	"fmt"
 	"net"
 	"syscall"
+	"time"
 
 	current "github.com/containernetworking/cni/pkg/types/100"
 	"github.com/vishvananda/netlink"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv6"
+	"golang.org/x/sys/unix"
 )
 
 var (
@@ -195,4 +197,39 @@ func AnnounceIPs(ifName string, ipConfigs []*current.IPConfig) error {
 		}
 	}
 	return nil
+}
+
+// Blocking wait for interface ifName to have carrier (!NO_CARRIER flag).
+func WaitForCarrier(ifName string, waitTime time.Duration) bool {
+	var sleepDuration time.Duration
+
+	myNetLink := MyNetlink{}
+
+	start := time.Now()
+
+	for time.Since(start) < waitTime {
+		if sleepDuration == 0 {
+			sleepDuration = 2 * time.Millisecond
+		} else {
+			time.Sleep(sleepDuration)
+			/* Grow wait time exponentionally (factor 1.5). */
+			sleepDuration += sleepDuration / 2
+		}
+
+		linkObj, err := myNetLink.LinkByName(ifName)
+		if err != nil {
+			return false
+		}
+
+		/* Wait for carrier, i.e. IFF_UP|IFF_RUNNING. Note that there is also
+		 * IFF_LOWER_UP, but we follow iproute2 ([1]).
+		 *
+		 * [1] https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/tree/ip/ipaddress.c?id=f9601b10c21145f76c3d46c163bac39515ed2061#n86
+		 */
+		if linkObj.Attrs().RawFlags&(unix.IFF_UP|unix.IFF_RUNNING) == (unix.IFF_UP | unix.IFF_RUNNING) {
+			return true
+		}
+	}
+
+	return false
 }